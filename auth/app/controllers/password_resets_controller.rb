8d0d3d4526db67c19ced039a63e35a8e716ef61c
David Silver <dsilver@candidatemetrics.com>
2012-08-29 05:10:33 -0700

    add password reset to User

--- auth/app/controllers/password_resets_controller.rb
+++ auth/app/controllers/password_resets_controller.rb
@@ -4,7 +4,7 @@ class PasswordResetsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-    UserMailer.password_reset(user).deliver
+    user.send_password_reset
     redirect_to :root, notice: 'Email sent with password reset instructions.'
   end
 end
--- auth/db/schema.rb
+++ auth/db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110613044221) do
+ActiveRecord::Schema.define(:version => 20120829120637) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",             :null => false
+    t.datetime "updated_at",             :null => false
+    t.string   "password_reset_token"
+    t.datetime "password_reset_sent_at"
   end
 
 end
