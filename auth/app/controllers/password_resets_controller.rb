f4f7cfb1024708d026a50e65204312ee30cca390
David Silver <dsilver@candidatemetrics.com>
2012-08-29 05:21:47 -0700

    don't send email to an invalid user

--- auth/app/controllers/password_resets_controller.rb
+++ auth/app/controllers/password_resets_controller.rb
@@ -4,7 +4,7 @@ class PasswordResetsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-    user.send_password_reset
+    user.send_password_reset if user
     redirect_to :root, notice: 'Email sent with password reset instructions.'
   end
 end
